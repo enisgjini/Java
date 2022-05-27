9e62a03e824d086ce91a25f70c90bd90200efb7c
Enis Gjini <101415557+enisgjini@users.noreply.github.com>
2022-05-27 15:09:10 +0200

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Student
+ */
+import java.util.*;
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       
+//       int count = 10;
+//       int j = 0;
+//       for (int i = 0;i < count;i++)
+//       {
+//           
+//           j++;
+//           System.out.println(j-1);
+//       }
+       
+       int count = 10;
+       for (int i = 0;i< count;i++)
+       {
+           
+           System.out.println(i);
+       }
+              
+               
+    }
+    
+}
